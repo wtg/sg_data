c437aed741f4ab7e836e9c912ca226646a2884a1
Justin Etzine <justetz@gmail.com>
2017-09-16 16:15:46 -0400

    Added listening notice

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ connection.sync().then(() => {
 
     app.use(require('./routes')(connection))
 
-    app.listen(process.env.PORT || 3000, process.env.IP || 'localhost')
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('Listening on port ' + (process.env.PORT || 3000))
+    })
 })
