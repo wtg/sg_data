fc65d97d30ef8bc1de439662afe662df038a9858
Justin Etzine <justetz@gmail.com>
2017-09-16 16:28:23 -0400

    Route listing

--- routes.js
+++ routes.js
@@ -24,5 +24,24 @@ module.exports = connection => {
         let resource = epilogue.resource(configuration)
     }
 
+    router.get('/', (req, res) => {
+        var output = ''
+        for (var key in router.stack) {
+            if (router.stack.hasOwnProperty(key)) {
+                var val = router.stack[key]
+                if(val.route) {
+                    val = val.route
+                    if(val.stack[0].method === 'get') {
+                        output += `<li><a href="${val.path}">${val.path} (${val.stack[0].method})</a></li>`
+                    } else {
+                        output += `<li>${val.path} (${val.stack[0].method})</li>`
+                    }
+                }
+            }
+        }
+
+        res.send('<ul>' + output + '</ul>')
+    })
+
     return router
 }
